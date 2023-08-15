3c2140fc65e6a81b36f1fc04ce01d9d2c44d2afa
Joubert Natividade <joubert.devba@gmail.com>
2023-08-14 22:10:42 -0300

    creating project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Header } from "./components/Header"
+import "./global.css"
+
+
+function App() {
+ 
+
+  return (
+    <>
+          <Header />
+    <h1>Hello</h1>
+    </>
+  )
+}
+
+export default App
